f432d4633cff16c68e53f15a789f56e1bf705a45
platisam <platisam@gmail.com>
2023-08-29 15:46:34 +0200

    adding reset all and increment amount buttons

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { decrement, increment } from "./counterSlice";
+import { decrement, increment, incrementByAmount, reset } from "./counterSlice";
 import { useState } from "react";
 
 const Counter = () => {
@@ -8,12 +8,32 @@ const Counter = () => {
 
   const [incrementAmount, setIncrementAmount] = useState(0);
 
+  const addValue = Number(incrementAmount) || 0;
+
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  };
+
   return (
     <section>
       <p>{count}</p>
       <div>
         <button onClick={() => dispatch(increment())}>+</button>
         <button onClick={() => dispatch(decrement())}>-</button>
+      </div>
+
+      <input
+        type="text"
+        value={incrementAmount}
+        onChange={(e) => setIncrementAmount(e.target.value)}
+      />
+
+      <div>
+        <button onClick={() => dispatch(incrementByAmount(addValue))}>
+          Add Amount
+        </button>
+        <button onClick={resetAll}>Reset</button>
       </div>
     </section>
   );
